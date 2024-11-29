cc94cec79fa559f61c9fc7b347c7f948afb16f20
LuckyCharlie29 <62743080+LuckyCharlie29@users.noreply.github.com>
2024-11-29 13:05:24 +0100

    Update Series.jsx

--- src/componentes/Series.jsx
+++ src/componentes/Series.jsx
@@ -4,17 +4,17 @@ import './general.css';
 function Series({ volver }) {
   const articulos = [
     {
-      titulo: 'Cancelan el anime de Transformers por recortes en presupuesto',
-      descripcion: 'Ahora yo...',
-      categoria: 'Series de moda',
+      titulo: 'Squid game temporada 2 a estrenarse el 26 de diciembre',
+      descripcion: 'Ya queda menos de un mes para el estreno de la segunda temporada de la famosa serie',
+      categoria: 'Nuevos estrenos',
       fecha: 'Hace 1 hora',
       autor: 'Álvaro Pérez',
       comentarios: '3 comentarios',
-      imagen: require('../recursos/42zardfyztqd1.jpeg'),
+      imagen: require('../recursos/squid-game-season-2-new-key-art-and-teaser-images_vfyn.jpg'),
     },
     {
-      titulo: 'The Walking Dead: Daryl Dixon: La nueva serie de la saga',
-      descripcion: '¿Qué podemos esperar de la primera temporada?',
+      titulo: 'The Walking Dead: Daryl Dixon',
+      descripcion: 'Nueva Serie',
       categoria: 'Series',
       fecha: 'Hace 3 horas',
       autor: 'Álvaro Pérez',
@@ -22,13 +22,13 @@ function Series({ volver }) {
       imagen: require('../recursos/c1bb5f6f4a814f2ce4b46b9ebcbf57c649f87b2b06b115b0148e1fbf5dbbdf6a._BR-6_AC_SX720_FMjpg_.jpg'),
     },
     {
-      titulo: 'Nuevo anime de donald trump en producción',
-      descripcion: 'La serie constará de donald trump.',
-      categoria: 'Series a estrenarse',
+      titulo: 'Ya ha empezado el rodaje de la 5 temporada de The Boys',
+      descripcion: 'No se sabe nada todavia de el rodaje.',
+      categoria: 'Nuevos estrenos',
       fecha: 'Hace 6 horas',
       autor: 'Álvaro Pérez',
       comentarios: '8 comentarios',
-      imagen: require('../recursos/animetrump-raises-fist-after-shooting.jpg'),
+      imagen: require('../recursos/dest_1.jpg'),
     },
   ];
 
@@ -54,11 +54,11 @@ function Series({ volver }) {
               <div key={index} className="articulo">
                 <img src={articulo.imagen} alt={articulo.titulo} className="articulo-foto"/>
                   <h3 className="articulo-titulo">{articulo.titulo}</h3>
-                  {/*<p className="articulo-descripcion">{articulo.descripcion}</p>
+                  <p className="articulo-descripcion">{articulo.descripcion}</p>
                   <span className="articulo-categoria">{articulo.categoria}</span>
                   <div className="articulo-detalles">
                   <span>{articulo.fecha}</span> - <span>{articulo.autor}</span> - <span>{articulo.comentarios}</span>
-                  </div>*/}
+              </div>
             </div>
           ))}
         </div>
@@ -70,3 +70,4 @@ function Series({ volver }) {
 }
 
 export default Series;
+
